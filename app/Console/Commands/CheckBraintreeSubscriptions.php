c5bd2de2ea19bdb4a790956d90ad8226a5b3e50e
airondevt <airondev@gmail.com>
2024-01-20 11:25:15 +0100

    fix on braintree subscription cron job

--- app/Console/Commands/CheckBraintreeSubscriptions.php
+++ app/Console/Commands/CheckBraintreeSubscriptions.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use App\Models\Plan;
 use App\Models\Subscription;
 use App\Traits\PaymentGateway;
+use Illuminate\Support\Facades\Log;
 
 class checkBraintreeSubscriptions extends Command
 {
@@ -37,36 +38,42 @@ class checkBraintreeSubscriptions extends Command
         //
 
         // Loop subscriptions
-        foreach(Subscription::all() as $subscription){
+        foreach (Subscription::all() as $subscription) {
 
             // Fetch subscription from braintree
-            $sub = $this->showSubscription($subscription);
+            try {
+                if ($sub = $this->showSubscription($subscription)) {
+                    // Update local values
+                    $subscription->plan_id = $sub->planId;
+                    $subscription->balance = $sub->balance;
+                    $subscription->billingDayOfMonth = $sub->billingDayOfMonth;
+                    $subscription->firstBillingDate = $sub->firstBillingDate;
+                    $subscription->nextBillingDate = $sub->nextBillingDate;
+                    $subscription->billingPeriodStartDate = $sub->billingPeriodStartDate;
+                    $subscription->billingPeriodEndDate = $sub->billingPeriodEndDate;
+                    $subscription->paidThroughDate = $sub->paidThroughDate;
+                    $subscription->currentBillingCycle = $sub->currentBillingCycle;
+                    $subscription->numberOfBillingCycles = $sub->numberOfBillingCycles;
+                    $subscription->neverExpires = $sub->neverExpires;
+                    $subscription->daysPastDue = $sub->daysPastDue;
+                    $subscription->failureCount = $sub->failureCount;
+                    $subscription->addOns = json_encode($sub->addOns);
+                    $subscription->discounts = json_encode($sub->discounts);
+                    $subscription->status = $sub->status;
+                    $subscription->statusHistory = json_encode($sub->statusHistory);
+                    $subscription->transactions = json_encode($sub->transactions);
+                    $subscription->save();
 
-            // Update local values
-            $subscription->plan_id = $sub->planId;
-            $subscription->balance = $sub->balance;
-            $subscription->billingDayOfMonth = $sub->billingDayOfMonth;
-            $subscription->firstBillingDate = $sub->firstBillingDate;
-            $subscription->nextBillingDate = $sub->nextBillingDate;
-            $subscription->billingPeriodStartDate = $sub->billingPeriodStartDate;
-            $subscription->billingPeriodEndDate = $sub->billingPeriodEndDate;
-            $subscription->paidThroughDate = $sub->paidThroughDate;
-            $subscription->currentBillingCycle = $sub->currentBillingCycle;
-            $subscription->numberOfBillingCycles = $sub->numberOfBillingCycles;
-            $subscription->neverExpires = $sub->neverExpires;
-            $subscription->daysPastDue = $sub->daysPastDue;
-            $subscription->failureCount = $sub->failureCount;
-            $subscription->addOns = json_encode($sub->addOns);
-            $subscription->discounts = json_encode($sub->discounts);
-            $subscription->status = $sub->status;
-            $subscription->statusHistory = json_encode($sub->statusHistory);
-            $subscription->transactions = json_encode($sub->transactions);
-            $subscription->save();
-
-            // If status is not active, update the web app site
-            if($subscription->status != 'Active'){
-                // Disable store on app
-                $response = apiCall( 'store/'.$subscription->user()->first()->store_id, $type = 'PUT', [ 'active' => false ]);
+                    // If status is not active, update the web app site
+                    if ($subscription->status != 'Active') {
+                        // Disable store on app
+                        // $response = apiCall( 'store/'.$subscription->user()->first()->store_id, $type = 'PUT', [ 'active' => false ]);
+                    }
+                }
+            } catch (\Throwable $th) {
+                //throw $th;
+                // fail gracefully for cron job methods
+                Log::error($th->getMessage(), []);
             }
         }
     }
